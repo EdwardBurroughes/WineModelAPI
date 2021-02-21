e13f5559395a0b37d45bda9fb06d76f1d57e759e
Edward Burroughes <burroughesedward@gmail.com>
2021-02-21 15:59:08 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+#TODO add all features whether to inlcude
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+
+setup(name='wine_predict_model',
+      version="1.0",
+      description="Wine Predict Model",
+      packages=find_packages(),
+      test_suite = 'tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      install_requires=requirements,
+      zip_safe=False)
