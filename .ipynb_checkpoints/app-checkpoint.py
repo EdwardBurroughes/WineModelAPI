c8cd7c57f319d60c0b96422893bdb1fd4b7908a0
Edward Burroughes <burroughesedward@gmail.com>
2021-02-24 07:56:58 +0000

    amendments

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -33,7 +33,8 @@ def predict_wine_rating():
                 variety=[variety],
                 winery=[winery]
                 ))
-    feat, model = load_models()
+    feat = joblib.load('model/feature_eng.joblib')
+    model = joblib.load('model/model.joblib')
     feat_eng_x = feat.transform(df)
     prediction = model.predict(feat_eng_x)
     return {'prediction':str(prediction[0])}
